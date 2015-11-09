4f9ce272ff9f6630388585fdde6506fff63c0e03
gputnam <graylcputnam@college.harvard.edu>
2015-11-09 15:18:39 -0500

    Added comment model

--- tutorial/blog/models.py
+++ tutorial/blog/models.py
@@ -6,3 +6,8 @@ class Blog(models.Model):
     text = models.TextField()
     author = models.CharField(max_length=30)
 
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog)
+    text = models.TextField()
+    author = models.CharField(max_length=30)
+
